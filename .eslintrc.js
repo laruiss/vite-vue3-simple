975dca2f799f91586bf010f7555cec9c03ac00a0
Stanislas Ormières <stan@stormier.ninja>
2021-12-08 10:51:17 +0100

    chore: :wrench: Configure ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:vue/vue3-recommended",
+        "standard"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "comma-dangle": ["error", "always-multiline"]
+    }
+};
